3fd47508550838a9a079a8c27f85e69cb3207768
Wayne Crouch <wayne.crouch@affiliatewindow.com>
2013-03-31 13:17:09 +0100

    Initial commit

---
+++ src/Wgcrouch/BehatExporter/Extension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wgcrouch\BehatExporter;
+
+use Symfony\Component\Config\FileLocator;
+use Behat\Behat\Extension\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+class Extension extends Extension
+{
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/services'));
+        $loader->load('core.yml');
+
+    }
+}
